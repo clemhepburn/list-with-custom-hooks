b4585bde7f44abcd0f7039e37a69f0a8ad879e0c
Clem Hepburn <clemhepburn@gmail.com>
2021-08-19 16:57:10 -0700

    adjust api to map

--- src/services/animalCrossingAPI.js
+++ src/services/animalCrossingAPI.js
@@ -1,12 +1,11 @@
 export const fetchVillagers = async () => {
   const res = await fetch('https://ac-vill.herokuapp.com/villagers');
   const json = await res.json();
-  return json;
-};
 
-export const fetchVillagersById = async (_id) => {
-  const res = await fetch(`https://ac-vill.herokuapp.com/villagers/${_id}`);
-  const json = await res.json();
-  return json;
+  return json.map((villager) => ({
+    japaneseName: villager.japaneseName,
+    image: villager.image,
+    phrase: villager.phrase,
+    _id: villager._id
+  }));
 };
-
