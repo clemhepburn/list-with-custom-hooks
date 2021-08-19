b4528b28d17b596aaa3d9cf997fcee40f371f065
Clem Hepburn <clemhepburn@gmail.com>
2021-08-19 16:40:00 -0700

    add router to App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../Home'
+import Header from '../Header'
 import Villagers from '../../containers/Villagers';
 import './App.css';
 
 export default function App() {
-  return <Villagers />;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/villagers" component={Villagers} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
