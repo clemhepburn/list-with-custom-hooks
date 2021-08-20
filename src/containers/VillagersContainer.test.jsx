dc546bd874c12a8dd3e4373f25844c4ddc1e1a78
Clem Hepburn <clemhepburn@gmail.com>
2021-08-19 17:07:43 -0700

    container test passes

--- src/containers/VillagersContainer.test.jsx
+++ src/containers/VillagersContainer.test.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import VillagersContainer from './Villagers';
+
+describe('Villagers Containter', () => {
+  it('should display animal crossing villagers', async () => {
+    render(<VillagersContainer />);
+
+    const ul = await screen.findByRole('list', { name: 'villagers' });
+    return waitFor(() => {
+      expect(ul).not.toBeEmptyDOMElement();
+    });
+  });
+});
